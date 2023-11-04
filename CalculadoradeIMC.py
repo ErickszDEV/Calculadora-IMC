8a2b7c658445073d723e65afe8db50e58fc3d642
Sourcery AI <>
2023-11-04 02:59:43 +0000

    'Refactored by Sourcery'

--- CalculadoradeIMC.py
+++ CalculadoradeIMC.py
@@ -2,7 +2,7 @@ print ("Bem vindo a Calculadora de IMC")
 
 pergunta_nome = input("Qual é o seu nome? " )
 nome = ("Olá" , pergunta_nome)
-idade = input (f"Qual sua idade? ")
+idade = input("Qual sua idade? ")
 peso = float(input("Qual seu peso (em KG)? "))
 altura = float(input("Qual sua Altura (em metros e usando ponto)? "))
 
